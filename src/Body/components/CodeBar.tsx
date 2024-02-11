5c864adc2948833c5f2c6dc4b186bfc06037bfc6
Rafael <rgarcia@flipoeyewear.com>
2024-02-11 23:36:59 +0100

    codeBar ready

--- src/Body/components/CodeBar.tsx
+++ src/Body/components/CodeBar.tsx
@@ -1,17 +1,18 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Image } from "react-native";
 import {
     widthToDP as wp,
     heightToDP as hp,
     widthToFonts as wf,
     heightToFonts as hf,
-  } from "react-native-responsive-screens";
+} from "react-native-responsive-screens";
 
 const CodeBar = () => {
     return (
         <View style={styles.container}>
-                <View style={styles.codeBar}>
-
-                </View>
+            <Image
+                style={styles.codeBar}
+                source={require('./../../../assets/barcode.gif')}
+            />
         </View>
     );
 
@@ -19,16 +20,16 @@ const CodeBar = () => {
 
 const styles = StyleSheet.create({
     container: {
-        display: 'flex', 
-        flexDirection: 'column-reverse', 
-        alignItems: 'center', 
+        display: 'flex',
+        flexDirection: 'column-reverse',
+        alignItems: 'center',
         height: hp('20%')
     },
     codeBar: {
         backgroundColor: 'pink',
         width: 145,
-        height: 55, 
-        marginBottom: hp('5%'), 
+        height: 55,
+        marginBottom: hp('5%'),
     },
 })
 
